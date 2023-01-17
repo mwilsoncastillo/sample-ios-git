e22b52138d8b0230405e67c1f8dc621c41742477
mwilsoncastillo <mwilsoncastillo@gmail.com>
2023-01-17 13:58:57 +0800

    initial commit ios

---
+++ sample-ios-git/sample-ios-git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sample-ios-git
+//
+//  Created by Mark Wilson Castillo on 1/16/23.
+//  Copyright © 2023 Mark Wilson Castillo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample-ios-git/sample-ios-gitTests/sample_ios_gitTests.swift
@@ -0,0 +1,34 @@
+//
+//  sample_ios_gitTests.swift
+//  sample-ios-gitTests
+//
+//  Created by Mark Wilson Castillo on 1/16/23.
+//  Copyright © 2023 Mark Wilson Castillo. All rights reserved.
+//
+
+import XCTest
+@testable import sample_ios_git
+
+class sample_ios_gitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
